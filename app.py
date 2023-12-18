c353b59c5211dddb8e01fec9730ab5b2f9762bca
kzsr0w0 <wkym0508@example.com>
2023-12-18 20:57:01 +0900

    14th commit

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg"])
 if uploaded_file is not None:
     # FastAPIエンドポイントに画像を送信
     #response = requests.post("http://127.0.0.1:8000/transfer-style/", files={"content_file": uploaded_file})
-    response = requests.post("/transfer-style/", files={"content_file": uploaded_file})
+    response = requests.post("https://xmasf.onrender.com/", files={"content_file": uploaded_file})
+    
     # 結果を表示
     if response.ok:
         st.image(response.content, caption='Styled Image', use_column_width=True)
